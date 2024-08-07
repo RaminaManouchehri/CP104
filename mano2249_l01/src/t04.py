b3421f5b532d344e5535011eb6bc42eb5b024c47
RaminaManouchehri <mano2249@mylaurier.ca>
2024-08-07 15:45:35 -0400

    l01

---
+++ mano2249_l01/src/t04.py
@@ -0,0 +1,18 @@
+"""
+-------------------------------------------------------
+[Lab 1, Task 4]
+-------------------------------------------------------
+Author:  Ramina Manouchehri
+ID:      169042249
+Email:   mano2249@mylaurier.ca
+__updated__ = "2022-09-17"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+name = input("Please enter your name: ")
+print("Pleased to meet you ")
+print(name)
---
+++ mano2249_l01/src/t06.py
@@ -0,0 +1,20 @@
+"""
+-------------------------------------------------------
+[Lab 1, Task 6]
+-------------------------------------------------------
+Author:  Ramina Manouchehri
+ID:      169042249
+Email:   mano2249@mylaurier.ca
+__updated__ = "2022-09-17"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+# Calculate total pay
+salary = 2500.0
+bonus = 1200.0
+pay = salary + bonus
+print('Your pay is', pay)
